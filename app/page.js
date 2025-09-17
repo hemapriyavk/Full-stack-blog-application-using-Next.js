dcdb9c9d2eb5db67a8e01b242547c55edca3c0b9
hemapriyavk <hemapriya2000mdu@gmail.com>
2025-09-17 20:09:48 +0530

    created blog list for home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+'use client'
+import BlogList from "@/Components/BlogList";
 import Header from "@/Components/Header";
 
 export default function Home() {
   return (
     <>
     <Header />
+    <BlogList />
     </>
   );
 }
