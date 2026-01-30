3b4f09ee55e1fc5f6ba9df22bfb693d79fe72ce5
Hemapriya <hemapriya@wealwin.com>
2026-01-30 15:46:06 +0530

    Create Blog List Page for Admin

---
+++ Components/AdminComponents/BlogTableItem.jsx
@@ -0,0 +1,29 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const BlogTableItem = ({ authorImg, title, author, date }) => {
+    const blogDate = new Date(date);
+    console.log('!!!!! authorImg', authorImg);
+    return (
+        <tr className='bg-white border-b'>
+            <th scope='row' className='items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
+
+                <Image width={40} height={40} alt='author-img' src={authorImg ? authorImg : assets?.profile_icon} />
+                <p>{author ? author : "No author"}</p>
+            </th>
+            <td className='px-6 py-4'>
+                {title ? title : "No title"}
+            </td>
+            <td className='px-6 py-4'>
+                {blogDate ? blogDate.toDateString() : '-'}
+            </td>
+            <td className='px-6 py-4 cursor-pointer'>
+                x
+            </td>
+
+        </tr>
+    )
+}
+
+export default BlogTableItem
